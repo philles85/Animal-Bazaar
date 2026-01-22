a8112d389f98bb1889726efbf04994ab5355abd4
philles85 <philster04@outlook.com>
2026-01-22 19:20:33 +0100

    Background image and some work on main

--- Frontend/FrontendFunctions.js
+++ Frontend/FrontendFunctions.js
@@ -1,5 +1,8 @@
+let register_popup = document.querySelector("#register_container_popup")
+
+
 function change_page(page) {
-    let all_pages = [start_page, pet_page, check_out_page, account_page];
+    let all_pages = [start_page, pet_page, check_out_page, account_page, register_popup];
 
     for (let pages of all_pages) {
         if (page == pages) {
