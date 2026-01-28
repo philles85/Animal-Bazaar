8bd16222c57374535c6e8f91ed3416f513371fff
Linus Strandberg <linusestrandberg@gmail.com>
2026-01-28 20:35:45 +0100

    Login popup
    Added login popup and created useable input fields

--- Frontend/Client.js
+++ Frontend/Client.js
@@ -5,7 +5,14 @@ const register = document.querySelector("#register_button");
 const register_container = document.querySelector("#register_container_popup")
 const close_register_button = document.querySelector("#close_register_popup_button");
 
+const close_login_button = document.querySelector("#close_login_popup_button")
+
+const login_container = document.querySelector("#login_container_popup")
+
 register.addEventListener("click", () => {
+    if(!login_container.classList.contains('hide')) {
+        change_visibility(login_container)
+    }
     change_visibility(register_container)
 })
 
@@ -13,3 +20,13 @@ close_register_button.addEventListener("click", () => {
     change_visibility(register_container)
 })
 
+log_in.addEventListener("click", () => {
+    if(!register_container.classList.contains('hide')) {
+        change_visibility(register_container)
+    }
+    change_visibility(login_container)
+})
+
+close_login_button.addEventListener("click", () => {
+    change_visibility(login_container)
+})
